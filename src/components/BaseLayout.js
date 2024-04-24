0450bcb6e52b49c56b429069f757be3400f30833
Miaoday <Starryvero@gmail.com>
2024-04-25 00:18:13 +0200

    test route

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -37,8 +37,8 @@ export default function BaseLayout() {
             <Grid item flexGrow={1}>
                <Routes>
                   <Route exact path={'/'} element={<Home/>}/>
-                  <Route exact path={'/aboutMe'} element={<About/>}/>
-                  <Route exact path={'/works'} element={<Portfolio/>}/>
+                  <Route exact path={'/about'} element={<About/>}/>
+                  <Route exact path={'/portfolio'} element={<Portfolio/>}/>
                </Routes>
             </Grid>
             <Grid item>
